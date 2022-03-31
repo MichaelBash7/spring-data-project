b918e14af0bc5fe109aa42dbf988221d41c7d021
MichaelBash7 <bash-misha@mail.ru>
2022-03-31 12:41:36 +0300

    add Services and Transactional

--- src/main/java/ru/itsjava/SpringDataApplication.java
+++ src/main/java/ru/itsjava/SpringDataApplication.java
@@ -1,46 +1,24 @@
 package ru.itsjava;
 
 
-import ru.itsjava.domain.Pet;
-import ru.itsjava.repository.PetRepository;
-import ru.itsjava.repository.UserRepository;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import ru.itsjava.service.PetService;
+import ru.itsjava.service.UserService;
 
-
-import java.sql.SQLException;
 
 
 @SpringBootApplication
 public class SpringDataApplication {
 
-    public static void main(String[] args) throws SQLException {
-        ApplicationContext context = SpringApplication.run(SpringDataApplication.class,args);
+    public static void main(String[] args) {
+        var context = SpringApplication.run(SpringDataApplication.class,args);
+        UserService userService = context.getBean(UserService.class);
+        userService.printAllUsers();
 
-        PetRepository petRepository = context.getBean(PetRepository.class);
-        System.out.println("petRepository.getById() = " + petRepository.getById(1L));
-
-        Pet pet = new Pet(0L, "Parrot");
-        petRepository.save(pet);
-        System.out.println("petRepository.getById(3L) = " + petRepository.getById(3L));
-
-        Pet pet3 = petRepository.getById(3L);
-        pet3.setSpecies("PARROT");
-        petRepository.save(pet3);
-        System.out.println("genreRepository.getById(3L) = " + petRepository.getById(3L));
-
-        petRepository.deleteById(3L);
-        System.out.println("genreRepository.findById(3L).isPresent() = " + petRepository.findById(3L).isPresent());
-
-        UserRepository userRepository = context.getBean(UserRepository.class);
-        System.out.println(userRepository.findAll());
-
-        System.out.println("petRepository.getBySpecies(\"PARROT\").isPresent() = " + petRepository.getBySpecies("PARROT").isPresent());
-
-        System.out.println("userRepository.findUserByNameAndPet(\"Misha\", petRepository.getById(1L)).isPresent() = " +
-                userRepository.findUserByNameAndPet("Misha", petRepository.getById(1L)).isPresent());
-
-
+        PetService petService = context.getBean(PetService.class);
+        petService.changePet("Dog", "Hamster");
+        petService.printPet("Hamster");
     }
 }
